981dbe217d1372a9daa3bbf4b7ceea4663d7aa93
Adorian16 <ionescu.adorian.mihai16@gmail.com>
2023-01-15 17:01:46 +0200

    munca din perioada 10-15.01.2023

---
+++ game.php
@@ -0,0 +1,47 @@
+<?php include('header.php');
+include 'db_connection.php';
+ 
+$game_ID=$_GET['IDGame'];
+
+$games_array=$conn->Query("SELECT * FROM games");
+
+    foreach($games_array as $game)
+{ $game_a=$game['IDGame'];
+    if($game_a == $game_ID ){
+    
+?>
+
+<div class="container">
+    <h1><?php echo $game['Name'];?></h1>
+</div>
+
+<div class="container text-center">
+            <div class="row">
+                <div class="col-4">
+
+                <img src="data:image;base64,<?php echo base64_encode($game["Picture"]);?>" class="card-img-top" alt="..." weight="300px"height="482.66px">
+                
+                </div>
+            </div>
+                
+        <div class="col-8">
+
+                <div class="text-start">
+                    
+                    <h3><?php echo $game['ReleaseYear'];?></h3>
+                    
+                </div>
+
+            <div class="text-start">
+                    <p class="fw-semibold"><?php echo $game['Description'];?></p><br>
+                    <p class="fw-semibold"><?php echo $game['Type'];?></p><br>
+                    <p class="fw-semibold"><?php echo $game['Description'];?></p><br>
+                    <button type="button" class="btn btn-warning"disabled><h1><?php echo $game['Price'];?> LEI</h1></button>
+            </div>
+        </div>
+
+</div>        
+<?php 
+}
+
+} ?>
--- index.php
+++ index.php
@@ -1,62 +1,29 @@
-<!DOCTYPE html>
-<html lang="en" data-bs-theme="dark">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>  
-    <title>Document</title>
-</head>
-<body>
-    
-    <nav class="navbar navbar-expand-lg bg-dark" >
-  <div class="container-fluid">
-    <img src="img/Ado_s.ico.ico" alt="Logo" width="70" height="70" class="d-inline-block align-text-top">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link mb-0 h1" href="index.html">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link mb-0 h1" href="#">Games</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link mb-0 h1" href="#">About Us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link mb-0 h1" href="#">Sing In</a>
-        </li>
-      </ul>
-      
-    </div>
-  </div>
-</nav>
+<?php include 'header.php';?>
 <br>
-<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="img/Design fără titlu (1).png" class="d-block w-100 p-4 c-img" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="img/Design fără titlu 1.png" class="d-block w-100 p-4 c-img" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="img/Design fără titlu 2.png" class="d-block w-100 p-4 c-img" alt="...">
-    </div>
+        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="img/Design fără titlu (1).png" class="d-block w-100 p-4 c-img" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="img/Design fără titlu 1.png" class="d-block w-100 p-4 c-img" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="img/Design fără titlu 2.png" class="d-block w-100 p-4 c-img" alt="...">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon-bg-light" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <span class="carousel-control-next-icon-bg-light" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+        
+       
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon-bg-light" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
-    <span class="carousel-control-next-icon-bg-light" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
 </div>
 </body>
 </html>
