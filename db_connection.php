c533735a8acc428279361bcd833edb1a561cdc46
Adorian16 <ionescu.adorian.mihai16@gmail.com>
2023-01-19 18:07:49 +0200

    am facut ultima modificare

--- db_connection.php
+++ db_connection.php
@@ -1,22 +1,19 @@
 <?php
-//database connection 'clienti'
+
 $servername = "localhost";
 $my_username = "root";
 $my_password = "";
 $dbname = "gamecorrner";
 
-
-
-// Create connection
 $conn = new mysqli($servername, $my_username, $my_password, $dbname);
 
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 $sql=("SELECT Picture FROM games");
 
 $result=$conn->query($sql);
+
 
 ?>
--- logout.php
+++ logout.php
@@ -1,9 +1,9 @@
 <?php
     session_start();
-    // Destroy session
+    
     session_unset();    
     if(session_destroy()) {
-        // Redirecting To Home Page
+        
         header("Location: index.php");
     }
 ?>
